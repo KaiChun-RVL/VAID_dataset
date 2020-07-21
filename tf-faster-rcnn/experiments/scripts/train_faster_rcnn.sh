74665ef5a780dcf48253642eb1ab2aab0cad1ba9
KaiChun-Tu <64946710+KaiChun-Tu@users.noreply.github.com>
2020-07-21 16:10:02 +0800

    iter_num

--- tf-faster-rcnn/experiments/scripts/train_faster_rcnn.sh
+++ tf-faster-rcnn/experiments/scripts/train_faster_rcnn.sh
@@ -19,7 +19,7 @@ case ${DATASET} in
     TRAIN_IMDB="voc_2007_trainval"
     TEST_IMDB="voc_2007_test"
     STEPSIZE="[50000]"
-    ITERS=10000
+    ITERS=70000
     ANCHORS="[8,16,32]"
     RATIOS="[0.5,1,2]"
     ;;
